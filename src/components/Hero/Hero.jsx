7b9aa59e4654a414a5a68778b33fd64865cceb2f
dhruv baheti <khivraj@dhruvs-MacBook-Pro-2.local>
2026-04-18 22:32:16 +0530

    added initial hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,24 @@
 import React from "react";
+import "../../styles/Hero.css";
 
 export default function Hero() {
-
   return (
-    <h1>This is Hero</h1>
-  )
+    <section className="hero">
+      <div className="hero-container">
+        
+        <h1 className="hero-title">
+          YC turns builders <br />into
+          <em> formidable founders</em>
+        </h1>
+
+        <p className="hero-quote">
+          [1] “A formidable person is one who seems like they'll get what they want,
+          regardless of whatever obstacles are in the way.”
+        </p>
+
+        <p className="hero-author">— Paul Graham</p>
+
+      </div>
+    </section>
+  );
 }
