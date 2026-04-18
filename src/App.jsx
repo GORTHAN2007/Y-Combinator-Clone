75f4132687aa18c628c3390ea88aaa7a06b431f0
amrutejr <abhinavamrute2007@gmail.com>
2026-04-18 20:29:49 +0530

    establishing connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
+import Hero from './components/Hero/Hero.jsx'
+import Footer from './components/Layout/Footer.jsx'
+import Navbar from './components/Layout/Navbar.jsx'
 
 function App() {
 
   return (
-    <h1>Hi</h1>
+    <>
+      <Navbar/>
+      <Hero/>
+      <Footer/>
+    </>
   )
 }
 
