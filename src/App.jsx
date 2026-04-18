c34d34b3c4d85ff3c38035ee2f2e589cc63c4620
Arju Srivastav <arju.srivastav7@gmail.com>
2026-04-18 20:43:37 +0530

    Added the css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
 import Hero from './components/Hero/Hero.jsx'
 import Footer from './components/Layout/Footer.jsx'
 import Navbar from './components/Layout/Navbar.jsx'
+import './styles/App.css'
 
 function App() {
 
