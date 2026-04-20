64402502f6662e428e70daa9336186af5c2dd042
Arju Srivastav <arju.srivastav7@gmail.com>
2026-04-20 16:00:08 +0530

    Added the basic version of Company Showcase with scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/Hero/Hero.jsx'
 import Footer from './components/Layout/Footer.jsx'
 import Navbar from './components/Layout/Navbar.jsx'
+import CompanyShowcase from './components/CompanyShowcase/CompanyShowcase.jsx'
 import './styles/App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar/>
       <Hero/>
+      <CompanyShowcase/>
       <Footer/>
     </>
   )
