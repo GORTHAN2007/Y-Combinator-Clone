59a09966b2ac853c591cdaf08ac2e8a082b57889
DhruvBaheti <dhruvbaheti52@gmail.com>
2026-04-25 23:31:07 +0530

    Added the Founders comment section and the history section

--- src/components/FoundersComments/FoundersComments.jsx
+++ src/components/FoundersComments/FoundersComments.jsx
@@ -1,9 +1,35 @@
-import '../../styles/FoundersComments.css';
+import "../../styles/FoundersComments.css";
+
+import Img1 from "../../assets/FoundersComments/FounderImg1.jpg";
+import Img2 from "../../assets/FoundersComments/FounderImg2.jpg";
+import Img3 from "../../assets/FoundersComments/FounderImg3.jpg";
+import Img4 from "../../assets/FoundersComments/FounderImg4.jpg";
+import Img5 from "../../assets/FoundersComments/FounderImg5.jpg";
 
 function FoundersComments() {
-    return(
-        <div>FoundersComments</div>
-    )
+  const images = [Img1, Img2, Img3, Img4, Img5];
+
+  return (
+    <section className="founders-comments-section">
+      
+      <div className="image-strip">
+        {images.map((img, i) => (
+          <div className="img-box" key={i}>
+            <img src={img} alt="" />
+          </div>
+        ))}
+      </div>
+
+      <div className="comments-wrapper">
+        <p className="small-title">IN FOUNDERS’ WORDS</p>
+
+        <p className="comment-text">
+          YC compresses months of growth into weeks. The sense of urgency is so infectious among founders that it becomes the most productive period in most people's lives. It's a community of founders that you can't find anywhere else. It feels like having the entire world at your back—from Partners to batchmates. Being surrounded by the top 1% of founders in the world just completely resets the bar. You leave with a completely new sense of how fast 'fast' can be.
+        </p>
+      </div>
+
+    </section>
+  );
 }
 
 export default FoundersComments;
--- src/components/History/History.jsx
+++ src/components/History/History.jsx
@@ -1,10 +1,26 @@
-import '../../styles/History.css';
+import "../../styles/History.css";
 
-function History(){
-    return(
-        <div>History</div>
-    )
+function History() {
+  return (
+    <section className="history-wrapper">
+      <div className="history-content">
+        <p>
+          In 2005, Y Combinator developed a new model of startup funding.
+          Four times a year we invest $500k in a select group of startups.
+          They move to San Francisco for three months, where we work
+          intensively with them to get the company into the best possible
+          shape, before they present to a large audience of investors on
+          Demo Day.
+        </p>
 
+        <p>
+          But YC doesn’t end on Demo Day. We and the YC alumni network
+          continue to help founders for the life of their company, and
+          beyond.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default History;
